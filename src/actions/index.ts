e68ad4e0e793dae2599b3afaa5c2460aae9001a5
Roman Valihura <roman.valihura@gmail.com>
2018-11-26 13:36:12 +0200

    add all exports to index action module file

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,1 +1,6 @@
-export * from './check'
+export * from './check.action'
+export * from './headers.action'
+export * from './process.action'
+export * from './report-ifspam.action'
+export * from './report.action'
+export * from './symbols.action'
