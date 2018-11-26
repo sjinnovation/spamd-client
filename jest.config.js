4f530ee1d9ed07972cc4ff7497af736beb272398
Roman Valihura <roman.valihura@gmail.com>
2018-11-26 17:24:55 +0200

    add config for tests

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  testRegex: '.?(spec).ts$',
+  coverageDirectory: './coverage/',
+  coveragePathIgnorePatterns: [
+    '.spec.ts',
+  ],
+  collectCoverageFrom : ['src/**/*.ts'],
+  moduleFileExtensions: [
+    'ts',
+    'js',
+    'json'
+  ],
+  globals: {
+    'ts-jest': {
+      tsConfigFile: './tsconfig.json',
+    }
+  },
+};
